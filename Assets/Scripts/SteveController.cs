b612ca149609c25dabaeffc385c7ae4c8385926e
Quinn Painter <quinn@geekymonkey.com>
2013-11-16 11:41:55 +0000

    First commit

---
+++ Assets/Scripts/SteveController.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SteveController : MonoBehaviour {
+
+	public float JumpSpeed = 350;
+	
+	void Update ()
+	{
+		if (Input.GetKeyDown(KeyCode.Space))
+		{
+			rigidbody.AddForce(0, JumpSpeed, 0);
+		}
+	}
+}
