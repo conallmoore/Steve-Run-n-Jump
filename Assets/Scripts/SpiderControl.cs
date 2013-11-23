d28bc2b40818b361a0e958469d7f9e7288c9e034
Quinn Painter <quinn@geekymonkey.com>
2013-11-23 16:22:24 +0000

    Made the guy jump properly

---
+++ Assets/Scripts/SpiderControl.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpiderControl : MonoBehaviour {
+
+	public float Speed = 10;
+
+	void FixedUpdate ()
+	{
+		rigidbody.AddForce(Speed * -1, 0, 0);
+	}
+}
--- Assets/Scripts/SteveController.cs
+++ Assets/Scripts/SteveController.cs
@@ -1,15 +1,28 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class SteveController : MonoBehaviour {
-
-	public float JumpSpeed = 350;
+public class SteveController : MonoBehaviour
+{
+	public float JumpSpeed = 250;
+	public bool OnGround = true;
 	
 	void Update ()
 	{
 		if (Input.GetKeyDown(KeyCode.Space))
 		{
-			rigidbody.AddForce(0, JumpSpeed, 0);
+			if (OnGround == true)
+			{
+				OnGround = false;
+				rigidbody.AddForce(0, JumpSpeed, 0);
+			}
+		}
+	}
+
+	void OnCollisionEnter(Collision collision)
+	{
+		if (collision.gameObject.tag == "Ground")
+		{
+			OnGround = true;
 		}
 	}
 }
