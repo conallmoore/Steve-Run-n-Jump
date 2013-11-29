0d4c93bb7534dad35decd4c55975ffcd45b63cd1
Quinn Painter <quinn@geekymonkey.com>
2013-11-29 20:03:43 +0000

    Added proper spiders and a spider spitter

---
+++ Assets/Scripts/SpiderSpitter.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpiderSpitter : MonoBehaviour {
+
+	public Transform SpiderPrefab;
+	public float SpiderWaitTime = 2;
+	private float WaitTime = 0;
+
+	// Use this for initialization
+	void Start ()
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		WaitTime += Time.deltaTime;
+		if (WaitTime > SpiderWaitTime)
+		{
+			WaitTime = 0;
+			Instantiate(SpiderPrefab, this.transform.position, this.transform.rotation);
+		}
+	}
+}
