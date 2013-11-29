5459e5962f3b7ba05e3416db7646bdc7488d6f28
Quinn Painter <quinn@geekymonkey.com>
2013-11-29 20:16:49 +0000

    Destroy the spiders when they go off the screen

--- Assets/Scripts/SpiderControl.cs
+++ Assets/Scripts/SpiderControl.cs
@@ -3,10 +3,18 @@ using System.Collections;
 
 public class SpiderControl : MonoBehaviour {
 
+	//public Transform Player;
 	public float Speed = 10;
 
 	void FixedUpdate ()
 	{
 		rigidbody.AddForce(Speed * -1, 0, 0);
+	}
+	void Update ()
+	{
+		if (this.rigidbody.position.x < -20)
+		{
+			Destroy(this.gameObject);
+		}
 	}
 }
