d7ab02144a90833a865bc1ced15622f4ae0eb9fa
Quinn Painter <quinn@geekymonkey.com>
2014-01-01 12:06:56 +0000

    The spider wait time is random now

--- Assets/Scripts/SpiderSpitter.cs
+++ Assets/Scripts/SpiderSpitter.cs
@@ -5,7 +5,9 @@ public class SpiderSpitter : MonoBehaviour {
 
 	public Transform Player;
 	public Transform SpiderPrefab;
-	public float SpiderWaitTime = 2;
+	public float MinSpiderWaitTime = 0.5f;
+	public float MaxSpiderWaitTime = 2f;
+	private float CurrentSpiderWaitTime = 0f;
 	private float WaitTime = 0;
 
 	// Use this for initialization
@@ -18,9 +20,10 @@ public class SpiderSpitter : MonoBehaviour {
 	void Update ()
 	{
 		WaitTime += Time.deltaTime;
-		if (WaitTime > SpiderWaitTime)
+		if (WaitTime > CurrentSpiderWaitTime)
 		{
 			WaitTime = 0;
+			CurrentSpiderWaitTime = Random.Range(MinSpiderWaitTime, MaxSpiderWaitTime);
 			Transform newTransform = (Transform) Instantiate(SpiderPrefab, this.transform.position, this.transform.rotation);
 			SpiderControl newSpider = newTransform.gameObject.GetComponent<SpiderControl>();
 			newSpider.Player = Player;
